ea9db716ddbfe69582c47c4a9f85575894cc3e8a
Matyáš Vítek <100159609+matytyma@users.noreply.github.com>
2022-12-27 17:33:37 +0100

    Create UpdateChecker

--- src/main/java/me/matytyma/bedwars/Main.java
+++ src/main/java/me/matytyma/bedwars/Main.java
@@ -1,11 +1,13 @@
 package me.matytyma.bedwars;
 
+import me.matytyma.bedwars.config.ConfigHandler;
+import me.matytyma.bedwars.updates.UpdateChecker;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
-
 	@Override
 	public void onEnable() {
-
+		ConfigHandler.loadConfig();
+		getServer().getScheduler().runTaskTimer(this, new UpdateChecker(), 0, 90000);
 	}
 }
---
+++ src/main/java/me/matytyma/bedwars/config/ConfigHandler.java
@@ -0,0 +1,17 @@
+package me.matytyma.bedwars.config;
+
+public class ConfigHandler {
+	public static final String PLUGIN_VERSION = "1.0.0";
+	private static String pluginPrefix = "[BedWars]";
+
+	/**
+	 * Loads all the plugin configuration from config.yml
+	 */
+	public static void loadConfig() {
+		
+	}
+
+	public static String getPluginPrefix() {
+		return pluginPrefix;
+	}
+}
