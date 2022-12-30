3621c9cb133b7dcfcf3e795d13aab1029dbe3f1e
Matyáš Vítek <100159609+matytyma@users.noreply.github.com>
2022-12-30 09:55:25 +0100

    Edit update-message style

--- src/main/java/me/matytyma/bedwars/updates/UpdateChecker.java
+++ src/main/java/me/matytyma/bedwars/updates/UpdateChecker.java
@@ -3,7 +3,6 @@ package me.matytyma.bedwars.updates;
 import com.google.gson.Gson;
 import me.matytyma.bedwars.config.ConfigHandler;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 
 import java.io.IOException;
 import java.net.URL;
@@ -16,13 +15,14 @@ public class UpdateChecker implements Runnable {
 			LatestRelease latestRelease = getLatestRelease();
 			Bukkit.getLogger().info(String.format("%s Checking for updates...", ConfigHandler.getPluginPrefix()));
 			if (!latestRelease.getName().equals(ConfigHandler.PLUGIN_VERSION)) {
-				Bukkit.getLogger().warning("\n");
-				Bukkit.getLogger().warning("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫  BedWars  ┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
-				Bukkit.getLogger().warning("┃ You are not running the latest plugin version.                          ┃");
-				Bukkit.getLogger().warning("┃ You're on version " + ConfigHandler.PLUGIN_VERSION + ", but the latest version is " + latestRelease.getName() + ".               ┃");
-				Bukkit.getLogger().warning("┃ Please download the latest version from link below.                     ┃");
-				Bukkit.getLogger().warning("┃ " + latestRelease.getDownloadUrl() + " ┃");
-				Bukkit.getLogger().warning("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n\n");
+				Bukkit.getLogger().warning("");
+				Bukkit.getLogger().warning("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫  BedWars  ┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫");
+				Bukkit.getLogger().warning("┃ You are not running the latest plugin version.");
+				Bukkit.getLogger().warning("┃ You're on version " + ConfigHandler.PLUGIN_VERSION + ", but the latest version is " + latestRelease.getName() + ".");
+				Bukkit.getLogger().warning("┃ Please download the latest version from link below.");
+				Bukkit.getLogger().warning("┃ " + latestRelease.getDownloadUrl());
+				Bukkit.getLogger().warning("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫");
+				Bukkit.getLogger().warning("");
 			}
 		} catch (IOException e) {
 			throw new RuntimeException(e);
@@ -32,14 +32,14 @@ public class UpdateChecker implements Runnable {
 	static LatestRelease getLatestRelease() throws IOException {
 		URL url = new URL("https://api.github.com/repos/matytyma/BedWars/releases/latest");
 		Scanner scanner = new Scanner(url.openStream()).useDelimiter("\\A");
-		System.out.println(scanner.hasNext() ? scanner.next() : ConfigHandler.PLUGIN_VERSION);
-		return new Gson().fromJson(scanner.hasNext() ? scanner.next() : ConfigHandler.PLUGIN_VERSION , LatestRelease.class);
+		return new Gson().fromJson(scanner.hasNext() ? scanner.next() : "{\n  \"name\": " + ConfigHandler.PLUGIN_VERSION + "\n}", LatestRelease.class);
 	}
 
 
 }
 
 class LatestRelease {
+	@SuppressWarnings("unused")
 	private String name;
 
 	public String getDownloadUrl() {
