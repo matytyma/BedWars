013f50099b6d95c6c36707df322c58f85d74cd6d
Matyáš Vítek <100159609+matytyma@users.noreply.github.com>
2022-12-30 10:16:25 +0100

    Added plugin prefix

--- src/main/java/me/matytyma/bedwars/config/ConfigHandler.java
+++ src/main/java/me/matytyma/bedwars/config/ConfigHandler.java
@@ -1,16 +1,25 @@
 package me.matytyma.bedwars.config;
 
+import me.matytyma.bedwars.Main;
+import org.bukkit.configuration.file.FileConfiguration;
+
 public class ConfigHandler {
 	public static final String PLUGIN_VERSION = "1.0.0";
-	private static String pluginPrefix = "[BedWars]";
+	private static String pluginPrefix;
 
 	/**
 	 * Loads all the plugin configuration from config.yml
 	 */
 	public static void loadConfig() {
-		
+		FileConfiguration config = Main.getPlugin(Main.class).getConfig();
+		pluginPrefix = config.getString("plugin-prefix");
+
 	}
 
+	/**
+	 * Returns the prefix of the plugin which will be displayed in chat and console
+	 * @return Prefix of the plugin
+	 */
 	public static String getPluginPrefix() {
 		return pluginPrefix;
 	}
